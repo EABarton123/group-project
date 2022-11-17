aee1177c20d968d6df14369088cf0eba0d89b617
EABarton123 <83503987+EABarton123@users.noreply.github.com>
2022-11-17 18:26:48 +0000

    MVP complete

--- growscript.js
+++ growscript.js
@@ -16,54 +16,75 @@ function getAllVegetables() {
         "https://th.bing.com/th/id/OIP.arpvVVBDY9CDTL-FL9Y6lgHaE8?w=252&h=180&c=7&r=0&o=5&dpr=2&pid=1.7",
       link: "https://www.rhs.org.uk/vegetables/carrots/grow-your-own",
     },
+    {
+      name: "How to grow brussels sprouts",
+      instructions:
+        "From mid-May to early June, when the young plants are 10-15cm (4-6in) tall and have seven true leaves, transplant them to their final growing position: Choose a sunny site, sheltered from strong winds: Improve the soil by adding up to two bucketfuls of well-rotted manure per square metre/yard, and a high potassium general fertiliser, such as Vitax Q4, at a rate of three handfuls per square metre/yard. Water the plants well before moving them, and again afterwards: Space them 60cm (2ft) apart, with 75cm (2½ft) between rows: Water the plants regularly until they have settled in. Then water in dry weather, repeating every 10–14 days. In July, apply a top-dressing of nitrogen-rich fertiliser, such as dried poultry manure pellets (150g/5oz per square metre/yard).In September, mound soil around the base of the stems, to provide extra support.",
+      image:
+        "https://www.agrifarming.in/wp-content/uploads/2015/10/Brussles-Sprout-Cultivation.jpg",
+      link: "https://www.rhs.org.uk/vegetables/brussels-sprouts/grow-your-own",
+    },
+    {
+      name: "How to grow parsnips",
+      instructions:
+        "When seedlings are about 2.5cm (1in) high, thin out to leave one every 15cm (6in). Keep the soil weed free, hand weeding rather than hoeing close to the plants, to avoid damaging the top of the root. Keep the soil evenly moist to prevent roots from splitting.",
+      image:
+        "https://www.deliaonline.com/sites/default/files/quick_media/ingredient-vegetarian-parsnips-reversed.jpg",
+      link: "https://www.rhs.org.uk/vegetables/parsnips/grow-your-own",
+    },
   ];
 }
 
-const body = document.getElementById("grow");
-const searchElement = document.getElementById("grow-search");
+const container = document.querySelector(".container");
+const vegetableList = document.getElementById("grow-list");
+const vegetableSearch = document.getElementById("grow-search");
 const allVegetables = getAllVegetables();
-
-function loadVegetablesList(vegetables) {
-  const body = document.getElementById("grow");
-  vegetables.forEach((vegetable) => {
-    body.appendChild(createVegetableList(vegetable));
-  });
+function setup() {
+  displayInstructions(allVegetables);
+  selectInstructions(allVegetables);
 }
-
-loadVegetablesList(allVegetables);
-
-function createVegetableList(vegetable) {
-  const eachVegetableCard = document.createElement("section");
-
-  const vegetableNameHeader = document.createElement("h2");
-  vegetableNameHeader.innerText = vegetable.name; //name is ready
-
-  const vegetableInstructions = document.createElement("p");
-  vegetableInstructions.innerHTML = vegetable.instructions; //summary is ready
-
-  // Now, we will append them one by one
-  eachVegetableCard.appendChild(vegetableNameHeader);
-  eachVegetableCard.appendChild(vegetableInstructions);
-
-  const imgTag = document.createElement("img");
-  imgTag.src = vegetable.image;
-  eachVegetableCard.appendChild(imgTag);
-
-  const linkToRHS = document.createElement("a");
-  linkToRHS.href = vegetable.link;
-  linkToRHS.innerHTML = "Taken from RHS website, click here to see more";
-  eachVegetableCard.appendChild(linkToRHS);
-  return eachVegetableCard;
+//To display all recipes
+function displayInstructions(vegetables) {
+  container.innerHTML = "";
+  for (let vegetable of vegetables) {
+    const article = document.createElement("article");
+    const title = document.createElement("h2");
+    title.textContent = `${vegetable.name}`;
+    article.appendChild(title);
+    const image = document.createElement("img");
+    image.src = vegetable.image;
+    article.appendChild(image);
+    const instructions = document.createElement("p");
+    instructions.textContent = `How to grow: ${vegetable.instructions}`;
+    article.appendChild(instructions);
+    container.appendChild(article);
+  }
 }
-
-searchElement.addEventListener("input", function () {
-  const searchInput = searchElement.value.toLowerCase();
-  const filterResults = allVegetables.filter((vegetable) => {
-    return (
-      vegetable.name.toLowerCase().includes(searchInput) ||
-      vegetable.instructions.toLowerCase().includes(searchInput)
-    );
+// To select recipe from option list
+function selectInstructions(vegetables) {
+  vegetableList.innerHTML = "";
+  for (let vegetable of vegetables) {
+    const option = document.createElement("option");
+    const dropdownOption = `${vegetable.name}`;
+    option.value = dropdownOption;
+    option.innerText = dropdownOption;
+    vegetableList.appendChild(option);
+  }
+}
+//To populate selected recipe
+vegetableList.addEventListener("change", (e) => {
+  const selectedInstructions = e.target.value;
+  const selectedInstruction = allVegetables.filter((vegetable) => {
+    return vegetable.name === selectedInstructions;
   });
-  body.innerHTML = "";
-  loadVegetablesList(filterResults);
+  displayInstructions(selectedInstruction);
 });
+//To search specific recipe
+vegetableSearch.addEventListener("input", (e) => {
+  const matchingValue = e.target.value.toUpperCase();
+  const matchingInstructions = allVegetables.filter((vegetable) => {
+    return vegetable.name.toUpperCase().includes(matchingValue);
+  });
+  displayInstructions(matchingInstructions);
+});
+window.onload = setup();
--- recipescript.js
+++ recipescript.js
@@ -1,61 +1,84 @@
 function getAllRecipes() {
   return [
     {
-      name: "leek and potato soup",
+      name: "Leek and Potato Soup",
       ingredients:
         "water (for water saute), leeks, potatoes, dried thyme, bay leaf, vegetable stock",
       image:
         "https://healthylivingmarket.com/wp-content/uploads/2017/04/potatoleek.jpg",
     },
     {
-      name: "carrot omelette",
+      name: "Carrot Omelette",
       ingredients: "grated carrots, rapeseed oil, eggs, salt",
       image:
         "https://www.pregnancyeats.com/wp-content/uploads/2022/01/10-Carrot-Omelette-on-Plate-for-Breakfast_A-Medium.jpg",
     },
+    {
+      name: "Brussels sprout, hazelnut & leek pasta",
+      ingredients:
+        "rapeseed oil, 3 leeks, brussels sprouts, 2 garlic cloves, pasta, lemon zest, parsley, chopped hazelnuts",
+      image:
+        "https://assets.bonappetit.com/photos/57accbedf1c801a1038bc788/1:1/w_2626,h_2626,c_limit/rigatoni-with-brussels-sprouts-parmesan-lemon-and-leek.jpg",
+    },
+    {
+      name: "Pumpkin & parsnip cassoulet",
+      ingredients:
+        "olive oil, 2 onions, pumpkin, parsnip, garlic cloves, mixed beans, chopped tomatoes, vegetable stock, thyme, fresh breadcrumb",
+      image:
+        "https://www.thetimes.co.uk/imageserver/image/%2Fmethode%2Ftimes%2Fprodmigration%2Fweb%2Fbin%2F6eff021b-c69f-334f-bce5-c3345e0ac84a.jpg?crop=1500%2C1000%2C0%2C0",
+    },
   ];
 }
-
-const body = document.getElementById("recipes");
-const searchElement = document.getElementById("recipe-search");
+const container = document.querySelector(".container");
+const recipeList = document.getElementById("recipe-list");
+const recipeSearch = document.getElementById("recipe-search");
 const allRecipes = getAllRecipes();
-
-function loadRecipeList(recipes) {
-  const body = document.getElementById("recipes");
-  recipes.forEach((recipe) => {
-    body.appendChild(createRecipeList(recipe));
-  });
+function setup() {
+  displayRecipes(allRecipes);
+  selectRecipes(allRecipes);
 }
-
-loadRecipeList(allRecipes);
-
-function createRecipeList(recipe) {
-  const eachRecipeCard = document.createElement("section");
-
-  const recipeNameHeader = document.createElement("h2");
-  recipeNameHeader.innerText = recipe.name; //name is ready
-
-  const recipeIngredients = document.createElement("p");
-  recipeIngredients.innerHTML = recipe.ingredients; //summary is ready
-
-  // Now, we will append them one by one
-  eachRecipeCard.appendChild(recipeNameHeader);
-  eachRecipeCard.appendChild(recipeIngredients);
-
-  const imgTag = document.createElement("img");
-  imgTag.src = recipe.image;
-  eachRecipeCard.appendChild(imgTag);
-  return eachRecipeCard;
+//To display all recipes
+function displayRecipes(recipes) {
+  container.innerHTML = "";
+  for (let recipe of recipes) {
+    const article = document.createElement("article");
+    const title = document.createElement("h2");
+    title.textContent = `${recipe.name}`;
+    article.appendChild(title);
+    const image = document.createElement("img");
+    image.src = recipe.image;
+    article.appendChild(image);
+    const ingredients = document.createElement("p");
+    ingredients.textContent = `Ingredients: ${recipe.ingredients}`;
+    article.appendChild(ingredients);
+    container.appendChild(article);
+  }
 }
-
-searchElement.addEventListener("input", function () {
-  const searchInput = searchElement.value.toLowerCase();
-  const filterResults = allRecipes.filter((recipe) => {
-    return (
-      recipe.name.toLowerCase().includes(searchInput) ||
-      recipe.ingredients.toLowerCase().includes(searchInput)
-    );
+// To select recipe from option list
+function selectRecipes(recipes) {
+  recipeList.innerHTML = "";
+  for (let recipe of recipes) {
+    const option = document.createElement("option");
+    const dropdownOption = `${recipe.name}`;
+    option.value = dropdownOption;
+    option.innerText = dropdownOption;
+    recipeList.appendChild(option);
+  }
+}
+//To populate selected recipe
+recipeList.addEventListener("change", (e) => {
+  const selectedRecipe = e.target.value;
+  const selectedRp = allRecipes.filter((recipe) => {
+    return recipe.name === selectedRecipe;
   });
-  body.innerHTML = "";
-  loadRecipeList(filterResults);
+  displayRecipes(selectedRp);
 });
+//To search specific recipe
+recipeSearch.addEventListener("input", (e) => {
+  const matchingValue = e.target.value.toUpperCase();
+  const matchingRecipes = allRecipes.filter((recipe) => {
+    return recipe.name.toUpperCase().includes(matchingValue);
+  });
+  displayRecipes(matchingRecipes);
+});
+window.onload = setup();
